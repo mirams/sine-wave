413fad19eba9ab59244d66610595d16bb6103fb7
Gary Mirams <gary.mirams@gmail.com>
2018-01-23 18:52:35 +0000

    Add figures on 5-state Markov model performance.

---
+++ Figures/Supplement_H_wang_model_fit/figure_h15/plot_figure_3_results.py
@@ -0,0 +1,310 @@
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import matplotlib.patches as patches
+from matplotlib.ticker import FormatStrFormatter, ScalarFormatter
+import matplotlib as mpl
+mpl.style.use('classic') # Use Matplotlib v1 defaults (plot was designed on this!)
+mpl.rc('text', usetex=True)
+
+import numpy
+from os.path import join, exists
+import re
+import sys
+plt.switch_backend('pdf')
+plt.tick_params(axis='both', which='minor', labelsize=16)
+
+voltage_file = 'figure_3_a/figure_3_model_fit_protocol.txt'
+data = numpy.loadtxt(voltage_file, skiprows=0)
+all_time = data[:, 0]
+voltage = data[:,1]
+
+fig = plt.figure(0, figsize=(10,7), dpi=900)
+#fig.text(0.51, 0.9, r'{0}'.format('Title'), ha='center', va='center', fontsize=16)
+
+start_of_zoom_time = 4.  # seconds
+length_of_zoom_time = 2. # seconds
+low_zoom_current = -1.1  # nA
+high_zoom_current = 1.3  # nA
+
+########################################################
+## Full plot of sine wave voltage protocol and current
+########################################################
+gs1 = gridspec.GridSpec(2, 1, height_ratios=[1,2],top=1.0,bottom=0.55,left=0.0,right=0.6 )
+ax1 = fig.add_subplot(gs1[0])
+ax2 = fig.add_subplot(gs1[1], sharex=ax1)
+plt.setp(ax1.get_xticklabels(), visible=False)
+
+# Voltage trace
+#ax1.set_title('Summat', fontsize=14)
+ax1.set_ylabel('Voltage (mV)', fontsize=14)
+#ax1.set_xlabel('Time (s)')
+#plt.setp(ax.get_xticklabels(), visible=False)
+ax1.set_xlim([0, 8])
+ax1.set_ylim([-130, 65])
+
+#ax1.set_yticklabels([r'$-10$', r'$-5$', r'$0$', r'$5$', r'$10$', r'$15$', r'$20$'])
+#ax1.set_xticklabels([r'$10^{-3}$', r'$10^{-2}$', r'$10^{-1}$', r'$10^0$', r'$10^1$', r'$10^2$'])
+ax1.plot(all_time, voltage, color='k', lw=2)
+# Shade in the zoomed in region
+ax1.add_patch(
+              patches.Rectangle(
+                                (start_of_zoom_time, -130),     # (x,y)
+                                length_of_zoom_time,            # width
+                                60-(-130),                      # height
+                                edgecolor="none",
+                                facecolor="grey",
+                                alpha=0.2,
+                                clip_on=False
+                                )
+              )
+
+experiment_file = 'figure_3_a/figure_3_model_fit_experimental_data.txt'
+#file = open(experiment_file, 'r')
+data = numpy.loadtxt(experiment_file, skiprows=0)
+experimental_current = data[:,1]
+
+current_file ='figure_3_a/figure_3_model_fit_new_model_fit.txt'
+data = numpy.loadtxt(current_file, skiprows=0)
+fitted_simulated_current = data[:,1]
+
+# Current trace
+#ax2.set_title('Summat', fontsize=14)
+ax2.set_xlim([0, 8])
+lower_current = -3.2
+ax2.set_ylim([lower_current, 1.5])
+ax2.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+
+# Shade in the zoomed in region
+ax2.add_patch(
+             patches.Rectangle(
+                               (start_of_zoom_time, low_zoom_current),   # (x,y)
+                               length_of_zoom_time,                 # width
+                               high_zoom_current-low_zoom_current,  # height
+                               edgecolor="none",
+                               facecolor="grey",
+                               alpha=0.2,
+                               clip_on=False
+                               )
+             )
+[a] = ax2.plot(all_time, experimental_current,color='r',lw=1)
+[b] = ax2.plot(all_time, fitted_simulated_current,color=[0,0.45,0.74], lw=1)
+ax2.set_ylabel('Current (nA)', fontsize=14)
+ax2.set_xlabel('Time (s)', fontsize=14)
+
+ax2.legend([a,b], ["Experiment","Fitted model"], loc=8, handletextpad=0.1, columnspacing=1,
+           ncol=2, borderaxespad=1)
+
+# Add the little zoom patch between plots
+patch_vertices = numpy.array([[start_of_zoom_time,lower_current],[0,-4.45],[8,-4.45],[start_of_zoom_time+length_of_zoom_time,lower_current]])
+
+polygon1 = plt.Polygon(patch_vertices,
+                       closed=True,
+                       edgecolor="none",
+                       facecolor="grey",
+                       alpha=0.15,
+                       clip_on=False
+                       )
+ax2.add_artist(polygon1)
+
+########################################################
+## Plot zoomed in voltage protocol and current
+########################################################
+gs2 = gridspec.GridSpec(2, 1, height_ratios=[1,2],top=0.47, bottom=0.0,left=0.0,right=0.6)
+ax3 = fig.add_subplot(gs2[0])
+ax4 = fig.add_subplot(gs2[1], sharex=ax3)
+plt.setp(ax3.get_xticklabels(), visible=False)
+# Voltage zoom trace
+#ax3.set_title('Summat', fontsize=14)
+ax3.set_ylabel('Voltage (mV)', fontsize=14)
+
+#ax3.set_xlabel('Time (s)')
+#plt.setp(ax.get_xticklabels(), visible=False)
+ax3.set_xlim([start_of_zoom_time, start_of_zoom_time+length_of_zoom_time])
+ax3.set_ylim([-130, 65])
+#ax3.set_yticklabels([r'$-10$', r'$-5$', r'$0$', r'$5$', r'$10$', r'$15$', r'$20$'])
+#ax3.set_xticklabels([r'$10^{-3}$', r'$10^{-2}$', r'$10^{-1}$', r'$10^0$', r'$10^1$', r'$10^2$'])
+ax3.plot(all_time, voltage, color='k', lw=2)
+
+
+# Current zoom trace
+ax4 = plt.subplot(gs2[1])
+#ax4.set_title('Summat', fontsize=14)
+ax4.set_ylabel('Current (nA)', fontsize=14)
+ax4.set_xlabel('Time (s)', fontsize=14)
+ax4.set_xlim([start_of_zoom_time, start_of_zoom_time+length_of_zoom_time])
+ax4.set_ylim([low_zoom_current, high_zoom_current])
+#ax4.set_prop_cycle(cycler('color',color_cycle[:,4:5]) + cycler('lw',line_width_cycle[:,4:5]))
+ax4.plot(all_time, experimental_current,color='r',lw=1)
+[f] = ax4.plot(all_time, fitted_simulated_current,color=[0,0.45,0.74], lw=1)
+
+
+# Squeeze the voltage and current plots together
+gs1.update(hspace=0.0)
+gs2.update(hspace=0.0)
+
+################################################################################
+# PLOT THE SCHEMATIC - unfortunately it doesn't seem to import PDF, so use .png
+################################################################################
+gs4 = gridspec.GridSpec(1, 1, top=1, bottom=0.75, left=0.67, right=1.0)
+
+ax5 = fig.add_subplot(gs4[0,0])
+image = plt.imread('wang.png')
+im = ax5.imshow(image)
+ax5.axis('off')
+
+# Histograms of MCMC results
+gs3 = gridspec.GridSpec(4, 4, top=0.73, bottom=0.0,left=0.67,right=1.0)
+gs3.update(hspace=0.65)
+gs3.update(wspace=0.07)
+
+########################################################
+## Plot histograms
+########################################################
+
+data = numpy.loadtxt('figure_3_c/MCMC_likelihood_and_samples.txt', skiprows=0)
+
+likelihoods = data[:,0]
+max_likelihood_index = numpy.argmax(likelihoods)
+print('Maximum likelihood at index',max_likelihood_index)
+
+ax6 = fig.add_subplot(gs3[0,0])
+weights = numpy.ones_like(data[:,1])/float(len(data[:,1]))
+ax6.hist(data[:,1], 20, normed=0, weights=weights, facecolor='r', edgecolor = "none", alpha=0.75)
+ax6.set_ylabel('Probability')
+ax6.set_xlabel('$P_1$')
+ax6.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+
+ax7 = fig.add_subplot(gs3[0,1],sharey=ax6)
+weights = numpy.ones_like(data[:,2])/float(len(data[:,2]))
+ax7.hist(data[:,2], 20, normed=0, weights=weights, facecolor='r', edgecolor = "none", alpha=0.75)
+ax7.set_xlabel('$P_2$')
+
+ax8 = fig.add_subplot(gs3[0,2],sharey=ax6)
+weights = numpy.ones_like(data[:,3])/float(len(data[:,3]))
+ax8.hist(data[:,3], 20, normed=0, weights=weights, facecolor='r', edgecolor = "none", alpha=0.75)
+ax8.set_xlabel('$P_3$')
+ax8.set_ylim([0., .2]) # This needs to be on the LAST of the shared axis plots (otherwise gets overridden)
+
+ax9 = fig.add_subplot(gs3[0,3])
+weights = numpy.ones_like(data[:,4])/float(len(data[:,4]))
+ax9.hist(data[:,4], 20, normed=0, weights=weights, facecolor='r', edgecolor = "none", alpha=0.75)
+ax9.set_xlabel('$P_4$')
+
+ax10 = fig.add_subplot(gs3[1,0],sharey=ax9)
+weights = numpy.ones_like(data[:,5])/float(len(data[:,5]))
+ax10.hist(data[:,5], 20, normed=0, weights=weights, facecolor='r', edgecolor = "none", alpha=0.75)
+ax10.set_xlabel('$P_5$')
+ax10.set_ylabel('Probability')
+ax10.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+
+ax11 = fig.add_subplot(gs3[1,1],sharey=ax9)
+weights = numpy.ones_like(data[:,6])/float(len(data[:,6]))
+ax11.hist(data[:,6], 20, normed=0, weights=weights, facecolor='r', edgecolor = "none", alpha=0.75)
+ax11.set_xlabel('$P_6$')
+ax11.set_ylim([0., .2])
+
+ax12 = fig.add_subplot(gs3[1,2])
+weights = numpy.ones_like(data[:,7])/float(len(data[:,7]))
+ax12.hist(data[:,7], 20, normed=0, weights=weights, facecolor='r', edgecolor = "none", alpha=0.75)
+ax12.set_xlabel('$P_7$')
+
+ax13 = fig.add_subplot(gs3[1,3],sharey=ax12)
+weights = numpy.ones_like(data[:,8])/float(len(data[:,8]))
+ax13.hist(data[:,8], 20, normed=0, weights=weights, facecolor='r', edgecolor = "none", alpha=0.75)
+ax13.set_xlabel('$P_8$')
+
+ax14 = fig.add_subplot(gs3[2,0],sharey=ax12)
+weights = numpy.ones_like(data[:,9])/float(len(data[:,9]))
+ax14.hist(data[:,9], 20, normed=0, weights=weights, facecolor='r', edgecolor = "none", alpha=0.75)
+ax14.set_xlabel('$P_9$')
+ax14.set_ylabel('Probability')
+ax14.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+
+ax15 = fig.add_subplot(gs3[2,1],sharey=ax12)
+weights = numpy.ones_like(data[:,10])/float(len(data[:,10]))
+ax15.hist(data[:,10], 20, normed=0, weights=weights, facecolor='r', edgecolor = "none", alpha=0.75)
+ax15.set_xlabel('$P_{10}$')
+
+ax16 = fig.add_subplot(gs3[2,2],sharey=ax12)
+weights = numpy.ones_like(data[:,11])/float(len(data[:,11]))
+ax16.hist(data[:,11], 20, normed=0, weights=weights, facecolor='r', edgecolor = "none", alpha=0.75)
+ax16.set_xlabel('$P_{11}$')
+
+ax17 = fig.add_subplot(gs3[2,3],sharey=ax12)
+weights = numpy.ones_like(data[:,12])/float(len(data[:,12]))
+ax17.hist(data[:,12], 20, normed=0, weights=weights, facecolor='r', edgecolor = "none", alpha=0.75)
+ax17.set_xlabel('$P_{12}$')
+
+ax18 = fig.add_subplot(gs3[3,0],sharey=ax12)
+weights = numpy.ones_like(data[:,13])/float(len(data[:,13]))
+ax18.hist(data[:,13], 20, normed=0, weights=weights, facecolor='r', edgecolor = "none", alpha=0.75)
+ax18.set_xlabel('$P_{13}$')
+ax18.set_ylabel('Probability')
+ax18.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+
+ax19 = fig.add_subplot(gs3[3,1],sharey=ax12)
+weights = numpy.ones_like(data[:,14])/float(len(data[:,14]))
+ax19.hist(data[:,14], 20, normed=0, weights=weights, facecolor='r', edgecolor = "none", alpha=0.75)
+ax19.set_xlabel('$P_{14}$')
+
+ax20 = fig.add_subplot(gs3[3,2],sharey=ax12)
+weights = numpy.ones_like(data[:,15])/float(len(data[:,15]))
+ax20.hist(data[:,15], 20, normed=0, weights=weights, facecolor='r', edgecolor = "none", alpha=0.75)
+ax20.set_xlabel('$G_{Kr}$')
+ax20.set_ylim([0., .2])
+#ax14.xaxis.set_major_formatter(FormatStrFormatter('%5.4g'))
+#ax14.set_xticklabels(ax14.xaxis.get_majorticklabels(), rotation=90)
+
+# Don't show y tick labels on most of the histograms
+plt.setp(ax7.get_yticklabels(), visible=False)
+plt.setp(ax8.get_yticklabels(), visible=False)
+plt.setp(ax9.get_yticklabels(), visible=False)
+plt.setp(ax11.get_yticklabels(), visible=False)
+plt.setp(ax12.get_yticklabels(), visible=False)
+plt.setp(ax13.get_yticklabels(), visible=False)
+plt.setp(ax15.get_yticklabels(), visible=False)
+plt.setp(ax16.get_yticklabels(), visible=False)
+plt.setp(ax17.get_yticklabels(), visible=False)
+plt.setp(ax19.get_yticklabels(), visible=False)
+plt.setp(ax20.get_yticklabels(), visible=False)
+
+#xfmt = ScalarFormatter(useOffset=False)
+#xfmt.set_powerlimits((-3,3))
+
+axes_list = [ax6,ax7,ax8,ax9,ax10,ax11,ax12,ax13,ax14,ax15,ax16,ax17,ax18,ax19,ax20]
+for i in range(0,len(axes_list)):
+    ax = axes_list[i]
+    ax.locator_params(tight=True, nbins=5)
+    #ax.xaxis.set_major_formatter(xfmt)
+    ax.get_xaxis().get_major_formatter().set_useOffset(False)
+    ax.get_xaxis().get_major_formatter().set_scientific(True)
+    ax.get_xaxis().get_major_formatter().set_powerlimits([-3,1])
+    for label in ax.get_xticklabels():
+        label.set_rotation(305)
+        label.set_horizontalalignment("left")
+    ax.plot(data[max_likelihood_index,i+1], 0, 'kx', markersize=10, markeredgewidth=3, clip_on=False)
+    if i>9:
+        ax.get_xaxis().set_label_coords(+0.2,0.9)
+    else:
+        ax.get_xaxis().set_label_coords(+0.2,0.9)
+
+########################################################
+# Add subfigure text labels, relative to axes top left
+########################################################
+left_shift_for_panel_labels = -0.165
+ax1.text(left_shift_for_panel_labels, 1.05, 'A', verticalalignment='top', horizontalalignment='left',
+         transform=ax1.transAxes,fontsize=20, fontweight='bold')
+ax5.text(-0.175, 1.10, 'B', verticalalignment='top', horizontalalignment='left',
+         transform=ax5.transAxes,fontsize=20, fontweight='bold')
+ax3.text(left_shift_for_panel_labels, 1.05, 'D', verticalalignment='top', horizontalalignment='left',
+         transform=ax3.transAxes,fontsize=20, fontweight='bold')
+ax6.text(-0.75, 1.4, 'C', verticalalignment='top', horizontalalignment='left',
+         transform=ax6.transAxes,fontsize=20, fontweight='bold')
+
+
+########################################################
+# FINAL PLOT OUTPUT
+########################################################
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+plt.savefig('figure_3_wang.pdf', bbox_inches='tight', dpi=900, pad_inches=0.05)
---
+++ Figures/Supplement_H_wang_model_fit/figure_h17/plot_figure_4_results.py
@@ -0,0 +1,248 @@
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import matplotlib.patches as patches
+from matplotlib.ticker import FormatStrFormatter
+
+import matplotlib as mpl
+mpl.style.use('classic') # Use Matplotlib v1 defaults (plot was designed on this!)
+mpl.rc('text', usetex=True)
+
+from cycler import cycler
+import numpy
+from os.path import join, exists
+import re
+import sys
+plt.switch_backend('pdf')
+plt.tick_params(axis='both', which='minor', labelsize=16)
+
+model_prediction_colour = [0,0.45,0.74]
+
+fig = plt.figure(0, figsize=(8,12), dpi=900)
+#fig.text(0.51, 0.9, r'{0}'.format('Title'), ha='center', va='center', fontsize=16)
+
+gs = gridspec.GridSpec(5, 3, height_ratios=[3,3,3,3,3], width_ratios=[1,1,1] )
+
+left_alignment_for_panel_label = -1.00
+protocol_names = ['steady_activation','inactivation','deactivation']
+
+for column in range(0,3):
+    protocol = protocol_names[column]
+
+    # Plot voltage protocols
+    if column==0:
+        ax1 = plt.subplot(gs[0,column])
+        ax1_col0 = ax1
+        ax1.set_title('Steady Activation (Pr3)')
+    else:
+        ax1 = plt.subplot(gs[0,column], sharey=ax1_col0)
+        if column==1:
+            ax1.set_title('Inactivation (Pr4)')
+        elif column==2:
+            ax1.set_title('Deactivation (Pr5)')
+
+    # Load voltage data
+    voltage_file = 'figure_4_' + protocol + '/' + 'figure_4_' + protocol + '_protocol.txt'
+    data = numpy.loadtxt(voltage_file, skiprows=0)
+    all_time = data[:, 0]
+    voltages = data[:,1:]
+    ax1.plot(all_time,voltages,'k-',lw=0.75)
+    ax1.set_xlabel('Time (s)', fontsize=12)
+    ax1.set_ylim([-130, 65])
+    ax1.set_xlim([0, numpy.amax(all_time)])
+
+    # Plot voltage protocols
+    ax2 = plt.subplot(gs[1,column])
+
+    # Experimental data
+    data_file = 'figure_4_' + protocol + '/' + 'figure_4_' + protocol + '_experiment.txt'
+    data = numpy.loadtxt(data_file, skiprows=0)
+    all_time = data[:, 0]
+    experimental_currents = data[:,1:]
+    ax2.plot(all_time,experimental_currents,'r-',lw=0.5)
+    ax2.set_xlabel('Time (s)', fontsize=12)
+
+    # Simulation
+    ax3 = plt.subplot(gs[2,column],sharex=ax2,sharey=ax2)
+    data_file = 'figure_4_' + protocol + '/' + 'figure_4_' + protocol + '_prediction.txt'
+    data = numpy.loadtxt(data_file, skiprows=0)
+    all_time = data[:, 0]
+    simulated_currents = data[:,1:]
+    ax3.plot(all_time,simulated_currents,'-',color=model_prediction_colour,lw=0.8)
+    ax3.set_xlabel('Time (s)', fontsize=12)
+
+    if column==0:
+        ax1.set_ylabel('Voltage\n(mV)', fontsize=14,rotation=0)
+        ax2.set_ylabel('Experimental\nCurrent (nA)', fontsize=14,rotation=0)
+        ax3.set_ylabel('Predicted\nCurrent (nA)', fontsize=14,rotation=0)
+        start_of_zoom_time = 0.6
+        length_of_zoom_time = 5.9
+        ax2.set_ylim([-1,2])
+    elif column==2:
+        start_of_zoom_time = 2.4
+        length_of_zoom_time = 5.6
+        ax2.set_ylim([-3.5,2])
+    elif column==1:
+        start_of_zoom_time = 1.2
+        length_of_zoom_time = 0.3
+        ax2.set_ylim([-5,10])
+        ax2.locator_params(axis='x', nbins=4)
+
+    ax2.set_xlim([start_of_zoom_time,start_of_zoom_time+length_of_zoom_time])
+
+    # Put a zoom section on
+    lower_voltage, tmp = ax1.get_ylim()
+    tmp, upper_v_time = ax1.get_xlim()
+    voltage_at_next_axes = -208
+
+    patch_vertices = numpy.array([[start_of_zoom_time,lower_voltage],
+                                  [0,voltage_at_next_axes],
+                                  [upper_v_time,voltage_at_next_axes],
+                                  [start_of_zoom_time+length_of_zoom_time,lower_voltage]])
+
+    ax1.add_artist(plt.Polygon(patch_vertices,
+                           closed=True,
+                           edgecolor="none",
+                           facecolor="grey",
+                           alpha=0.15,
+                           clip_on=False
+                           )
+               )
+
+    # Shift axis labels
+    axes_list = [ax1, ax2, ax3]
+    for ax in axes_list:
+        if column==0:
+            ax.get_yaxis().set_label_coords(-0.6,0.30)
+        ax.get_xaxis().set_label_coords(+0.5,-0.19)
+
+    if column == 0:
+        # Add subfigure text labels, relative to axes top left
+        ax1.text(left_alignment_for_panel_label, 1.05, 'A', verticalalignment='top', horizontalalignment='left', transform=ax1.transAxes, fontsize=20, fontweight='bold')
+        ax2.text(left_alignment_for_panel_label, 1.05, 'B', verticalalignment='top', horizontalalignment='left', transform=ax2.transAxes, fontsize=20, fontweight='bold')
+        ax3.text(left_alignment_for_panel_label, 1.05, 'C', verticalalignment='top', horizontalalignment='left', transform=ax3.transAxes, fontsize=20, fontweight='bold')
+
+
+# Probably easier just to plot all the summary graphs without looping!
+
+def get_model_name(argument):
+    switcher = {
+        0: "tentusscher",
+        1: "mazhari",
+        2: "diveroli",
+        3: "wang",
+        4: "zeng",
+        5: "experiment",
+        6: "prediction",
+    }
+    return switcher.get(argument, "nothing")
+
+color_cycle = [[1,0,1], [0.47,0.67,0.19], 'c', [0.49,0.18,0.56],'DarkOrange','r',model_prediction_colour]
+line_width_cycle = [0.5,0.5,0.5,0.5,0.5,2,2]
+line_style_cycle = ['-','-','-','-','-','--','-']
+
+###########################
+# S.S. activation IV curve
+###########################
+ax4 = plt.subplot(gs[3,0])
+
+###########################
+# S.S. activation tau curve - not plotting this as it isn't something usually seen!
+###########################
+#ax5 = plt.subplot(gs[4,0])
+
+###########################
+# deactivation - deactivation tau curve
+###########################
+ax6 = plt.subplot(gs[3,2])
+
+###########################
+# deactivation - recovery from inactivtion tau curve
+###########################
+ax7 = plt.subplot(gs[4,2])
+
+###########################
+# inactivtion- instantaneous inactivation tau curve
+###########################
+ax8 = plt.subplot(gs[3,1])
+
+legend_entry = []
+
+for model_idx in range(0,7):
+    file_name = 'figure_4_steady_activation/figure_4_steady_activation_iv_curve/figure_4_steady_activation_iv_' + get_model_name(model_idx) +'.txt'
+    data = numpy.loadtxt(file_name,skiprows=0)
+    ax4.plot(data[:,0],data[:,1],'.'+line_style_cycle[model_idx],color=color_cycle[model_idx],lw=line_width_cycle[model_idx])
+    
+#    file_name = 'figure_4_steady_activation/figure_4_steady_activation_tau_v_curve/figure_4_steady_activation_tau_v_' + get_model_name(model_idx) +'.txt'
+#    data = numpy.loadtxt(file_name,skiprows=0)
+#    ax5.semilogy(data[:,0],data[:,1],'.'+line_style_cycle[model_idx],color=color_cycle[model_idx],lw=line_width_cycle[model_idx])
+
+    file_name = 'figure_4_deactivation/figure_4_deactivation_tau_v/figure_4_deactivation_tau_v_' + get_model_name(model_idx) +'.txt'
+    data = numpy.loadtxt(file_name,skiprows=0)
+    [a] = ax6.semilogy(data[:,0],data[:,1],'.'+line_style_cycle[model_idx],color=color_cycle[model_idx],lw=line_width_cycle[model_idx])
+    legend_entry.append(a)
+    
+    # Don't plot inactivation or instantaneous inactivation tau curves for TT or Zeng, simulated curves not comparable.
+    if (get_model_name(model_idx) != "tentusscher") and (get_model_name(model_idx) != "zeng"):
+        file_name = 'figure_4_inactivation/figure_4_instantaneous_inactivation_tau/figure_4_instantaneous_inactivation_tau_v_' + get_model_name(model_idx) +'.txt'
+        data = numpy.loadtxt(file_name,skiprows=0)
+        ax8.plot(data[:,0],data[:,1],'.'+line_style_cycle[model_idx],color=color_cycle[model_idx],lw=line_width_cycle[model_idx])
+
+    if (get_model_name(model_idx) != "tentusscher") and (get_model_name(model_idx) != "zeng"):
+        file_name = 'figure_4_deactivation/figure_4_inactivation_tau_v/figure_4_inactivation_tau_v_' + get_model_name(model_idx) +'.txt'
+        data = numpy.loadtxt(file_name,skiprows=0)
+        ax7.plot(data[:,0],data[:,1],'.'+line_style_cycle[model_idx],color=color_cycle[model_idx],lw=line_width_cycle[model_idx])
+
+
+ax4.set_xlabel('Voltage (mV)', fontsize=12)
+ax4.set_ylabel('Current\n(normalized)', fontsize=12)
+ax4.get_yaxis().set_label_coords(-0.26,0.5)
+ax4.get_xaxis().set_label_coords(0.5,-0.19)
+
+#ax5.set_xlabel('Voltage (mV)', fontsize=12)
+#ax5.set_ylabel(r'Time constant $\tau$ (ms)', fontsize=12)
+#ax5.set_ylim([8,10000])
+#ax5.get_yaxis().set_label_coords(-0.3,0.4)
+#ax5.get_xaxis().set_label_coords(0.5,-0.19)
+
+ax6.set_xlabel('Voltage (mV)', fontsize=12)
+ax6.set_ylabel(r'Deactivation $\tau$ (ms)', fontsize=12)
+ax6.get_yaxis().set_label_coords(-0.16,0.5)
+ax6.set_ylim([1,4000])
+ax6.get_xaxis().set_label_coords(0.5,-0.19)
+
+ax7.set_xlabel('Voltage (mV)', fontsize=12)
+ax7.set_ylabel(r'Recovery inact. $\tau$ (ms)', fontsize=12)
+ax7.get_yaxis().set_label_coords(-0.16,0.4)
+ax7.get_xaxis().set_label_coords(0.5,-0.19)
+
+ax8.set_xlabel('Voltage (mV)', fontsize=12)
+ax8.set_ylabel(r'Inactivation $\tau$ (ms)', fontsize=12)
+ax8.get_yaxis().set_label_coords(-0.16,0.5)
+ax8.get_xaxis().set_label_coords(0.5,-0.19)
+ax8.set_xlim([-50, 50])
+
+ax6.locator_params(axis='x', nbins=4)
+ax7.locator_params(axis='x', nbins=4)
+ax7.locator_params(axis='y', nbins=6)
+#ax5.locator_params(axis='y', nbins=6)
+ax8.locator_params(axis='y', nbins=6)
+
+
+ax4.text(-130, -0.4, 'Summary\nPlots', ha='center', fontsize=14)
+
+
+ax7.legend(legend_entry, ["ten Tusscher `04","Mazhari `01","Di Veroli `13","Wang `97","Zeng `95","Experiment", "New model"], title="Legend", bbox_to_anchor=(-2.7, 0, 2.35, 1.5), loc='lower left', handletextpad=0.5,borderpad=0.5,labelspacing=0.35,columnspacing=4.5, ncol=2, borderaxespad=0.,fontsize=12)
+
+
+ax4.text(left_alignment_for_panel_label, 1.05, 'D', verticalalignment='top', horizontalalignment='left',
+         transform=ax4.transAxes,fontsize=20, fontweight='bold')
+
+#legend = ax4.legend(loc='center', shadow=False)
+gs.update(wspace=0.35, hspace=0.4)
+#fig.set_tight_layout(True)
+#gs.tight_layout(fig, renderer=None, pad=0, h_pad=None, w_pad=None, rect=None)
+#plt.tight_layout()
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+plt.subplots_adjust(top=0.75, wspace=0.25)
+plt.savefig('figure_4_wang.pdf', bbox_inches='tight', dpi=900, pad_inches=0.05)
